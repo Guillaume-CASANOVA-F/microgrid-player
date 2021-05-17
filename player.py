5affa1958c666e3913ca10aee7a76316cdab1822
Guillaume-CASANOVA-F <83076449+Guillaume-CASANOVA-F@users.noreply.github.com>
2021-05-17 09:18:37 +0200

    Update player.py

--- player.py
+++ player.py
@@ -3,19 +3,15 @@
 import numpy as np
 import pulp
 
-Capa = 60
-Pmax = 10
-rho_c = 0.95
-rho_d = 0.95
-delta_t = 0.5
-#prices = 100*np.random.rand(48)
-#scenario_data = np.random.rand(73, 50)
-
 class Player:
 
     def __init__(self):
         # some player might not have parameters
-        self.parameters = 0
+        self.Capa = 60
+        self.Pmax = 10
+        self.rho_c = 0.95
+        self.rho_d = 0.95
+        self.delta_t = 0.5
         self.horizon = 48
 
     def set_scenario(self, scenario_data):
@@ -47,12 +43,12 @@ class Player:
             #stock = delta_t * pulp.lpSum([ (rho_c * variables[s]["battery_load_+"] - (variables[s]["battery_load_-"] * (1/rho_d) ) ) for s in range(t) ] )
 
             constraint_name = "stock_positif" + str(t)
-            my_lp_problem += delta_t * pulp.lpSum([ (rho_c * variables[s]["battery_load_+"] - (variables[s]["battery_load_-"] * (1/rho_d) ) ) for s in range(t) ] ) >= 0, constraint_name
+            my_lp_problem += self.delta_t * pulp.lpSum([ (self.rho_c * variables[s]["battery_load_+"] - (variables[s]["battery_load_-"] * (1/self.rho_d) ) ) for s in range(t) ] ) >= 0, constraint_name
 
             constraint_name = "stock_ne_depasse_pas_la_capacite" + str(t)
-            my_lp_problem += delta_t * pulp.lpSum([ (rho_c * variables[s]["battery_load_+"] - (variables[s]["battery_load_-"] * (1/rho_d) ) ) for s in range(t) ] ) <= Capa, constraint_name
+            my_lp_problem += self.delta_t * pulp.lpSum([ (self.rho_c * variables[s]["battery_load_+"] - (variables[s]["battery_load_-"] * (1/self.rho_d) ) ) for s in range(t) ] ) <= self.Capa, constraint_name
 
-        my_lp_problem.setObjective(pulp.lpSum( [ ( ( self.prices[t] - (rho_c * rho_d * delta_t * self.prices[self.horizon-1]) ) * variables[t]["battery_load_+"] ) - ( self.prices[t] - ( delta_t * self.prices[self.horizon-1] ) * variables[t]["battery_load_-"] )  for t in range(self.horizon)] ))
+        my_lp_problem.setObjective(pulp.lpSum( [ ( ( self.prices[t] - (self.rho_c * self.rho_d * self.delta_t * self.prices[self.horizon-1]) ) * variables[t]["battery_load_+"] ) - ( self.prices[t] - ( self.delta_t * self.prices[self.horizon-1] ) * variables[t]["battery_load_-"] )  for t in range(self.horizon)] ))
         my_lp_problem.solve()
 
         battery_load = []
@@ -75,10 +71,11 @@ class Player:
     def reset(self):
         # reset all observed data
         pass
-
-"""
-Industrial_consumer = Player()
-Industrial_consumer.set_prices(prices)
-Industrial_consumer.set_scenario(scenario_data)
-print(Industrial_consumer.compute_all_load())
-"""
+    
+if __name__ == "__main__":
+    prices = 100*np.random.rand(48)
+    scenario_data = np.random.rand(73, 50)
+    Industrial_consumer = Player()
+    Industrial_consumer.set_prices(prices)
+    Industrial_consumer.set_scenario(scenario_data)
+    print(Industrial_consumer.compute_all_load())
