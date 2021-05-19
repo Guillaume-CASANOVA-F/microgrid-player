619124f6435a70d81ffdbe7ee4f7fbf1b7eb96be
Guillaume-CASANOVA-F <83076449+Guillaume-CASANOVA-F@users.noreply.github.com>
2021-05-19 16:53:05 +0200

    Update player.py

--- player.py
+++ player.py
@@ -16,6 +16,7 @@ class Player:
         self.rho_d = 0.95
         self.delta_t = 0.5
         self.horizon = 48
+        self.stock_initial = 0
 
     def set_scenario(self, scenario_data):
         self.data = scenario_data
@@ -43,7 +44,7 @@ class Player:
             var_name = "battery_load_moins" + str(t)
             variables[t]["battery_load_moins"] = pulp.LpVariable(var_name, 0, self.Pmax)
 
-            stock = self.delta_t * pulp.lpSum([(self.rho_c * variables[s]["battery_load_plus"] - (
+            stock = self.stock_initial + self.delta_t * pulp.lpSum([(self.rho_c * variables[s]["battery_load_plus"] - (
                     variables[s]["battery_load_moins"] * (1 / self.rho_d))) for s in
                                                range(t+1)])
 
@@ -53,18 +54,16 @@ class Player:
             constraint_name = "stock_ne_depasse_pas_la_capacite" + str(t)
             my_lp_problem += stock <= self.Capa, constraint_name
 
-        #my_lp_problem.setObjective(pulp.lpSum([((self.prices[t] - (
-        #            self.rho_c * self.rho_d * self.delta_t * self.prices[self.horizon - 1])) * variables[t][
-        #                                            "battery_load_plus"]) - (self.prices[t] - (
-        #            self.delta_t * self.prices[self.horizon - 1]) * variables[t]["battery_load_moins"]) for t in
-        #                                       range(self.horizon)]))
+        stock_final = self.stock_initial + self.delta_t * pulp.lpSum([(self.rho_c * variables[s]["battery_load_plus"] - (
+                    variables[s]["battery_load_moins"] * (1 / self.rho_d))) for s in
+                                               range(self.horizon)])
+
         my_lp_problem.setObjective( pulp.lpSum(
             [self.prices[t] * self.delta_t * (variables[t]["battery_load_plus"] - variables[t]["battery_load_moins"])
              for t in
-             range(self.horizon)]) - self.rho_d * stock * self.prices[self.horizon-1])
+             range(self.horizon)]) - self.rho_d * (stock_final - self.stock_initial) * self.prices[self.horizon-1])
 
         my_lp_problem.solve()
-
         battery_load = []
 
         for t in range(self.horizon):
@@ -88,10 +87,10 @@ class Player:
 
 
 if __name__ == "__main__":
-    #prices = 100 * np.random.rand(48)
-    prices = 100 * np.ones(48)
-    prices[0] = 10
-    prices[20] = 150
+    prices = 100 * np.random.rand(48)
+    #prices = 100 * np.ones(48)
+    #prices[0] = 50
+    #prices[3] = 120
     data = pd.read_csv(os.path.join(os.getcwd(), "indus_cons_scenarios.csv"), sep=";", decimal=".")
     scenario_data = np.array(data["cons (kW)"])
     Industrial_consumer = Player()
